44c174fc196b6fe360ee8a780e777cfefe4af0b5
paulwong888 <paulwong888@gmail.com>
2013-03-07 12:44:08 +0000

    Initial Project

--- leave-application-system/src/main/java/com/paul/leave/dao/LeaveDao.java
+++ leave-application-system/src/main/java/com/paul/leave/dao/LeaveDao.java
@@ -1,14 +1,8 @@
 package com.paul.leave.dao;
 
+import com.googlecode.genericdao.dao.hibernate.GenericDAO;
 import com.paul.leave.valueObject.Leave;
 
-public interface LeaveDao {
+public interface LeaveDao extends GenericDAO<Leave, String>{
 
-	void save(Leave leave);
-	
-	void update(Leave leave);
-	
-	void delete(Leave leave);
-	
-	Leave findByLeaveCode(String id);
 }
--- leave-application-system/src/main/java/com/paul/leave/dao/impl/LeaveDaoImpl.java
+++ leave-application-system/src/main/java/com/paul/leave/dao/impl/LeaveDaoImpl.java
@@ -1,17 +1,15 @@
 package com.paul.leave.dao.impl;
 
-import java.util.List;
-
 import org.springframework.stereotype.Repository;
 
-import com.paul.common.util.CustomHibernateDaoSupport;
+import com.paul.base.dao.BaseDao;
 import com.paul.leave.dao.LeaveDao;
 import com.paul.leave.valueObject.Leave;
 
 @Repository("leaveDao")
-public class LeaveDaoImpl extends CustomHibernateDaoSupport implements LeaveDao {
+public class LeaveDaoImpl extends BaseDao<Leave, String> implements LeaveDao {
 
-	public void save(Leave leave) {
+	/*public void save(Leave leave) {
 		getHibernateTemplate().save(leave);
 	}
 
@@ -26,6 +24,6 @@ public class LeaveDaoImpl extends CustomHibernateDaoSupport implements LeaveDao 
 	public Leave findByLeaveCode(String id) {
 		List<Leave> list = getHibernateTemplate().find("from Leave where id=?",id);
 		return (Leave)list.get(0);
-	}
+	}*/
 
 }
